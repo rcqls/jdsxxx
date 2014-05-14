68089b52edf6d42f7d7a6a015d30a69867de66ce
Nathalie Villa-Vialaneix <nathalie.villa@toulouse.inra.fr>
2014-05-14 16:00:54 +0200

    cherry-picked bootstrap formating of program from jds 2014
    Conflicts:
    
    	prog/details.php
    	prog/showabstract.php

--- prog/details.php
+++ prog/details.php
@@ -1,19 +1,48 @@
 <html dir="ltr" lang="fr-FR">
-<head>
-<meta charset="UTF-8" />
-<?php
-echo "<title>Programme | ".$eventname."</title>";
-?>
-</head>
-<body>
 
 <?php
 echo "<h2>Programme détaillé (par jour) ".$eventname."</h2>
       Choisissez le jour et vous pourrez visualiser le programme et les résumés courts et longs (à partir de la date limite de soumission des versions révisées).<br>
       Retour sur le <a href='".$website_url."'>site principal</a>.";
 ?>
-<br>
-<br>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Nathalie Villa-Vialaneix">
+<?php
+	echo "<meta name='description' content='Programme ".$eventname."'>";
+	echo "<title>Programme | ".$eventname."</title>";
+?>
+	<link rel="shortcut icon" href="img/favicon.ico">
+
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+    
+	<!--mathjax-->
+	<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+</head>
+
+<body>
+	<div class="container">
+
+<?php
+		echo "<h1>Programme détaillé ".$eventname."</h1>";
+?>
+
+		<div class="well">
+			<div class="media">
+				<a class="pull-left" href="#">
+					<img class="media-object" src="img/sfds.png" alt="SFdS" width="96">
+				</a>
+				<div class="media-body">
+	<?php
+					echo "Le programme détaillé par jour est disponible sur cette page. Vous avez également accès aux résumés courts et longs (à partir de la date limite de soumission des versions révisées). Retour sur le <a href='".$website_url."'>site principal de la manifestation</a>.";
+	?>
+				</div> <!-- media-body -->
+			</div> <!-- media -->
+		</div> <!-- well -->
 
 <?php
 // functions to deal with the date in English
@@ -27,102 +56,102 @@ function month_in_french($the_month) {
 }
 
 // database connexion
-require_once("../payment/config.php");
-require_once("../payment/connect.php");
+require_once("/payment/config.php");
+require_once("/payment/connect.php");
+
 $db=mysql_connect($db_server,$db_user,$db_pass) or die('Erreur de connexion '.mysql_error());
 mysql_select_db($db_user,$db) or die('Erreur de sélection '.mysql_error());
 
 $url_showabstract=$website_url."/prog/showabstract.php";
 if(isset($_POST['day'])) $sel_day=$_POST['day']; else $sel_day=0;
 
-echo "<form method='post' action='".$url_prog."' name='main'>";
 $dayquery = 'SELECT DISTINCT slot_date FROM `Slot` ORDER BY slot_date ASC';
 $dayres = mysql_query($dayquery) or die('Erreur SQL !'.$dayquery.'</br>'.mysql_error());
-if (mysql_num_rows($dayres)) {
-  echo "<select name='day'>";
-  while ($the_day=mysql_fetch_array($dayres)) {
-    echo "<option value='".$the_day['slot_date']."'>".day_in_french(date('l',strtotime($the_day['slot_date'])))." ".date('d',strtotime($the_day['slot_date']))." ".month_in_french(date('F',strtotime($the_day['slot_date'])))."</option>";
-  }
-  echo "</select>";
-}
-echo "<input type='submit' value='Choisir' />";
-echo "</form>";
+$dayres2 = mysql_query($dayquery) or die('Erreur SQL !'.$dayquery.'</br>'.mysql_error());
 
-if ($sel_day!='') {
-  $slotquery = 'SELECT C.id, C.slot_date, C.begin, C.end FROM `Slot` C' . ' WHERE C.slot_date="' . $sel_day . '" ORDER BY C.begin ASC';
-  $slotres = mysql_query($slotquery) or die('Erreur SQL !'.$slotquery.'</br>'.mysql_error());
-  echo "<center><h2>Programme du ".day_in_french(date('l',strtotime($sel_day)))." ".date('d',strtotime($sel_day))." ".month_in_french(date('F',strtotime($sel_day)))."</h2></center>";
+		echo '<ul class="pagination">';
+		while ($the_day=mysql_fetch_array($dayres2)) {
+			$sel_day=$the_day['slot_date'];
+			echo '<li><a href="#'.$sel_day.'">'.day_in_french(date('l',strtotime($sel_day))).'</a></li>';
+		}
+		echo '</ul>';
 
-if (mysql_num_rows($slotres)) {
-while ($the_slot=mysql_fetch_array($slotres)) {
-  $slotid = $the_slot['id'];
-  echo "<h2>".date('H',strtotime($the_slot['begin']))."h".date('i',strtotime($the_slot['begin']))."-"
-  .date('H',strtotime($the_slot['end']))."h".date('i',strtotime($the_slot['end']))."</h2>";
+		while ($the_day=mysql_fetch_array($dayres)) {
+			$sel_day=$the_day['slot_date'];
+			$slotquery = 'SELECT C.id, C.slot_date, C.begin, C.end FROM `Slot` C' . ' WHERE C.slot_date="' . $sel_day . '" ORDER BY C.begin ASC';
+			$slotres = mysql_query($slotquery) or die('Erreur SQL !'.$slotquery.'</br>'.mysql_error());
+			echo "<a name='".$sel_day."'></a><center><h2>Programme du ".day_in_french(date('l',strtotime($sel_day)))." ".date('d',strtotime($sel_day))." ".month_in_french(date('F',strtotime($sel_day)))."</h2></center>";
 
-  $sessionquery= 'SELECT S.name, S.room, S.chair, S.id FROM `ConfSession` S' . ' WHERE S.id_slot=' . $slotid . ' ORDER BY S.room ASC';
-  $sessionres=mysql_query($sessionquery) or die('Erreur SQL !'.$sessionquery.'</br>'.mysql_error());
+			if (mysql_num_rows($slotres)) {
+				while ($the_slot=mysql_fetch_array($slotres)) {
+					$slotid = $the_slot['id'];
+					echo "<h4>".date('H',strtotime($the_slot['begin']))."h".date('i',strtotime($the_slot['begin']))."-".date('H',strtotime($the_slot['end']))."h".date('i',strtotime($the_slot['end']))."</h4>";
+					$sessionquery= 'SELECT S.name, S.room, S.chair, S.id FROM `ConfSession` S' . ' WHERE S.id_slot=' . $slotid . ' ORDER BY S.room ASC';
+					$sessionres=mysql_query($sessionquery) or die('Erreur SQL !'.$sessionquery.'</br>'.mysql_error());
 
-  if (mysql_num_rows($sessionres)) {
-  while ($the_session=mysql_fetch_array($sessionres)) {
-    $sessionid = $the_session['id'];
-    echo "<center><b>".utf8_encode($the_session['name'])."</b> (".utf8_encode($the_session['room']).")<br />";
-    echo "Modérateur : <em>".utf8_encode($the_session['chair'])."</em></center>";
+					if (mysql_num_rows($sessionres)) {
+						while ($the_session=mysql_fetch_array($sessionres)) {
+							$sessionid = $the_session['id'];
+							echo "<center><b>".utf8_encode($the_session['name'])."</b> (".utf8_encode($the_session['room']).")<br />";
+							echo "Modérateur : <em>".utf8_encode($the_session['chair'])."</em></center>";
   
-    $papersquery = 'SELECT P.id, P.title, P.isUploaded FROM `Paper` P' . ' WHERE P.id_conf_session=' . $sessionid . ' ORDER BY P.position_in_session ASC';
-    $papersres = mysql_query($papersquery) or die('Erreur SQL !'.$papersquery.'</br>'.mysql_error());
-    $nb_paper = mysql_num_rows($papersres);
-    $the_slot['begin'];
-    $currentime = strtotime($the_slot['begin']);
-    if ($nb_paper>0) {
-      $duration = (date('H',strtotime($the_slot['end']))*60-date('H',$currentime)*60+date('i',strtotime($the_slot['end']))-date('i',$currentime))/$nb_paper;
-    } else $duration=0;
-    $curnb = $nb_paper;
+							$papersquery = 'SELECT P.id, P.title, P.isUploaded FROM `Paper` P' . ' WHERE P.id_conf_session=' . $sessionid . ' ORDER BY P.position_in_session ASC';
+							$papersres = mysql_query($papersquery) or die('Erreur SQL !'.$papersquery.'</br>'.mysql_error());
+							$nb_paper = mysql_num_rows($papersres);
+							$the_slot['begin'];
+							$currentime = strtotime($the_slot['begin']);
+							if ($nb_paper>0) {
+								$duration = (date('H',strtotime($the_slot['end']))*60-date('H',$currentime)*60+date('i',strtotime($the_slot['end']))-date('i',$currentime))/$nb_paper;
+							} else $duration=0;
+							$curnb = $nb_paper;
 
-    if (mysql_num_rows($papersres)) {
-    while ($the_paper=mysql_fetch_array($papersres)) {
-      if ($curnb==$nb_paper) {
-        echo "<font size=2>";
-      }
-      $paperid = $the_paper['id'];
-      echo "<em>".date('H',$currentime)."h".date("i",$currentime)."</em> : <b>".utf8_encode($the_paper['title'])."</b><br/>";
-      $currentime = $currentime+$duration*60;
-    
-      $authorquery = 'SELECT U.last_name, U.first_name, U.email, U.affiliation, A.position, P.emailContact '
-                     . ' FROM `Paper` P, `Author` A, `User` U '
-                     . ' WHERE P.id=A.id_paper AND A.id_user=U.id AND P.id=' . $paperid
-                     . ' ORDER BY A.position ASC';
-      $authorres = mysql_query($authorquery) or die('Erreur SQL ! '.$authorquery.'<br/>'.mysql_error());
+							if (mysql_num_rows($papersres)) {
+							while ($the_paper=mysql_fetch_array($papersres)) {
+								if ($curnb==$nb_paper) {
+									echo "<font size=2>";
+								}
+								$paperid = $the_paper['id'];
+								echo "<em>".date('H',$currentime)."h".date("i",$currentime)."</em> : <b>".utf8_encode($the_paper['title'])."</b><br/>";
+								$currentime = $currentime+$duration*60;
+							
+								$authorquery = 'SELECT U.last_name, U.first_name, U.email, U.affiliation, A.position, P.emailContact '
+															. ' FROM `Paper` P, `Author` A, `User` U '
+															. ' WHERE P.id=A.id_paper AND A.id_user=U.id AND P.id=' . $paperid
+															. ' ORDER BY A.position ASC';
+								$authorres = mysql_query($authorquery) or die('Erreur SQL ! '.$authorquery.'<br/>'.mysql_error());
 
-      $nb_aut = mysql_num_rows($authorres);
-      while ($the_author = mysql_fetch_array($authorres)) {
-        if ($the_author['email']===$the_author['emailContact']) echo "<b>";
-        echo utf8_encode($the_author['first_name'])." ".utf8_encode($the_author['last_name']);
-        if ($the_author['email']===$the_author['emailContact']) echo "</b>";
-        echo " (".utf8_encode($the_author['affiliation']).")";
-        $nb_aut = $nb_aut-1;
-        if ($nb_aut==0) {
-          echo "<br/>";
-        } else {
-          echo " ; ";
-        }
-      }
-      if (time() >= strtotime($deadline_papers)) {
-        if ($the_paper['isUploaded']=='Y') {
-          $filepath = $url_papers."submission_".$paperid.".pdf";
-          echo "<a href='".$filepath."' target='_blank'>Résumé long au format PDF</a> - ";
-        }
-        $abstractpath = $url_showabstract."?id=".$paperid;
-        echo "<a href='".$abstractpath."' target='_blank'>Résumé court</a><br />";
-      }
-      $curnb=$curnb-1;
-      if ($curnb==0) {
-        echo "</font>";
-      }
-    }}
-  }}
+								$nb_aut = mysql_num_rows($authorres);
+								while ($the_author = mysql_fetch_array($authorres)) {
+									if ($the_author['email']===$the_author['emailContact']) echo "<b>";
+									echo utf8_encode($the_author['first_name'])." ".utf8_encode($the_author['last_name']);
+									if ($the_author['email']===$the_author['emailContact']) echo "</b>";
+									echo " (".utf8_encode($the_author['affiliation']).")";
+									$nb_aut = $nb_aut-1;
+									if ($nb_aut==0) {
+										echo "<br/>";
+									} else {
+										echo " ; ";
+									}
+								}
+								if (time() >= strtotime($deadline_papers)) {
+									if ($the_paper['isUploaded']=='Y') {
+										$filepath = $url_papers."submission_".$paperid.".pdf";
+										echo "<a href='".$filepath."' target='_blank'>Résumé long au format PDF</a> - ";
+									}
+									$abstractpath = $url_showabstract."?id=".$paperid;
+									echo "<a href='".$abstractpath."' target='_blank'>Résumé court</a><br />";
+								}
+								$curnb=$curnb-1;
+								if ($curnb==0) {
+									echo "</font>";
+							}
+					}}
+			}}
 }}}
 mysql_close();
 ?>
-
-</body>
+		</div> <!-- container -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/bootstrap.js"></script>
+	</body>
 </html>
--- prog/showabstract.php
+++ prog/showabstract.php
@@ -1,13 +1,27 @@
+<html dir="ltr" lang="fr-FR">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Nathalie Villa-Vialaneix">
 <?php
+	echo "<meta name='description' content='Programme ".$eventname."'>";
+	echo "<title>Programme | ".$eventname."</title>";
+?>
+	<link rel="shortcut icon" href="img/favicon.ico">
+</head>
 
-require_once("../payment/config.php");
-require_once("../payment/connect.php");
+<body>
 
-function printsummary($paperid){
+<?php
+require_once("/payment/config.php");
+require_once("/payment/connect.php");
+
 // Connection to db: note that table must be identical to user
 $db=mysql_connect($db_server,$db_user,$db_pass) or die('Erreur de connexion '.mysql_error());
-mysql_select_db($db_user,$db) or die('Erreur de selection '.mysql_error());
+mysql_select_db($db_user,$db) or die('Erreur de sélection '.mysql_error());
 
+function printsummary($paperid){
 $sql = 'SELECT P.title, A.content , P.emailContact, U.affiliation '
         . ' FROM `Abstract` A, `Paper` P, `User` U '
         . ' WHERE A.id_paper=P.id AND P.emailContact=U.email AND A.id_paper=' . $paperid;
@@ -18,36 +32,39 @@ $sql2= 'SELECT U.last_name, U.first_name, U.email, U.affiliation, A.position '
         . ' WHERE P.id=A.id_paper AND A.id_user=U.id AND P.id=' . $paperid
         . ' ORDER BY A.position ASC';
 $result2=mysql_query($sql2) or die('Erreur SQL ! '.$sql2.'<br/>'.mysql_error());
-mysql_close();
 
-echo "<hr/>Résumé ".$paperid." :<br/><br/>";
-if(mysql_num_rows($result)){
-	while ($row = mysql_fetch_array($result)){
-		echo "<b>".$row['title']."</b><br/>";
-		$nb_aut = mysql_num_rows($result2);
-		while ($row2 = mysql_fetch_array($result2)){
-			if ($row2['email']===$row['emailContact']) echo "<u>";
-			echo $row2['last_name'].", ".$row2['first_name'];
-			if ($row2['email']===$row['emailContact']) echo "</u>";
-			$nb_aut = $nb_aut-1;
-			if ($nb_aut==0)
-				echo " <br/><em>".$row['affiliation']."</em><br/>";
-			else
-				echo " ; ";
+	echo "<hr/>Résumé ".$paperid." :<br/><br/>";
+	if(mysql_num_rows($result)){
+		while ($row = mysql_fetch_array($result)){
+			echo "<b>".$row['title']."</b><br/>";
+			$nb_aut = mysql_num_rows($result2);
+			while ($row2 = mysql_fetch_array($result2)){
+				if ($row2['email']===$row['emailContact']) echo "<u>";
+				echo $row2['last_name'].", ".$row2['first_name'];
+				if ($row2['email']===$row['emailContact']) echo "</u>";
+				$nb_aut = $nb_aut-1;
+				if ($nb_aut==0)
+					echo " <br/><em>".$row['affiliation']."</em><br/>";
+				else
+					echo " ; ";
+			}
+			echo "<br/>".htmlspecialchars_decode($row['content'])."<br/>";
 		}
-		echo "<br/>".htmlspecialchars_decode($row['content'])."<br/>";
+		$filepath = $url_papers."submission_".$paperid.".pdf";
+		if (file_exists($filepath)){
+			echo "<br/><center><a href='".$filepath."' target='_blank'>Résumé long au format PDF</a></center>";
+		}
+		echo "<hr/>";
 	}
-	$filepath = $url_papers."submission_".$paperid.".pdf";
-	if (file_exists($filepath)){
-		echo "<br/><center><a href='".$filepath."' target='_blank'>Résumé long au format PDF</a></center>";
+	else
+		echo "No such entry in database<br/><hr/>";
 	}
-	echo "<hr/>";
-}
-else
-	echo "No such entry in database<br/><hr/>";
-}
 
 $id=$_GET["id"];
 printsummary($id);
 
+mysql_close();
+
 ?>
+	</body>
+</html>
